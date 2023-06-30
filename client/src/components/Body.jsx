9097803767704ac4ef9be5de55c1da48cd1d393e
JimmyTrouble <114571635+JimmyTrouble@users.noreply.github.com>
2023-06-30 18:38:44 +1000

    added qr generation

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -3,6 +3,7 @@ import "./body.css";
 
 function Body() {
   const [inputValue, setInputValue] = useState("");
+  const [imageURL, setImageURL] = useState("");
 
   const handleSubmit = (event) => {
     console.log("clicked");
@@ -15,10 +16,11 @@ function Body() {
       body: JSON.stringify({ text: inputValue }), // And here is the data we're sending
     })
       .then((response) => response.text())
-      .then((data) => console.log(data))
+      .then((data) => setImageURL(data))
       .catch((error) => {
         console.error("Error", error);
       });
+    console.log(imageURL);
   };
   const handleChange = (event) => {
     setInputValue(event.target.value);
@@ -30,6 +32,7 @@ function Body() {
         <input placeholder="type" value={inputValue} onChange={handleChange} />
         <input type="submit" value="Submit" />
       </form>
+      {imageURL && <img src={imageURL} alt="qr-image" />}
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 import express from "express";
 import cors from "cors";
+import qr from "qr-image";
+import { createWriteStream } from "fs";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/qr-images", express.static("qr-images")); // add this line to serve images
 
 app.post("/", (req, res) => {
-  console.log(req.body);
-  res.status(200).send("OK");
+  console.log(req.body.text);
+  let inputText = req.body.text;
+  let qr_png = qr.image(inputText, { type: "png" });
+  let qr_path = `./qr-images/${inputText}.png`;
+  qr_png.pipe(createWriteStream(qr_path));
+  res.send(`http://localhost:3001/qr-images/${inputText}.png`); // Send the image URL path
 });
 
 app.get("/", (req, res) => {
